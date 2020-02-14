b0caaafb45687f9acae5e25df4d414ae6b70fe50
Wechuli <wechulipaul@yahoo.com>
2020-02-14 07:50:05 +0300

    select in channels

---
+++ programs/concurrency_in_go/select/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	startTime := time.Now().UnixNano() / 1000000000
+
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		c1 <- "Hello!"
+
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "Salut!"
+	}()
+
+	time.Sleep(time.Second * 2)
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("Received: ", msg1)
+		case msg2 := <-c2:
+			fmt.Println("Received: ", msg2)
+		default:
+			fmt.Println("No acitivity")
+
+		}
+	}
+
+	endTime := time.Now().UnixNano() / 1000000000
+
+	fmt.Println("Time for program to execute: ", endTime-startTime)
+
+}
